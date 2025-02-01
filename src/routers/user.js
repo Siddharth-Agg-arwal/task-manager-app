f18980a2f0636d7d81f88a58785fd8fe206d0621
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2025-02-01 13:20:17 +0530

    delete and fetch avatars

--- src/routers/user.js
+++ src/routers/user.js
@@ -141,7 +141,7 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest : 'avatars',
+    // dest : 'avatars', //only allows to store image on local directories
     limits :{
         fileSize : 1000000,
     },
@@ -154,10 +154,43 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), auth, async ( req, res) => {
+//router to add user image
+router.post('/users/me/avatar', auth, upload.single('avatar'), async ( req, res) => {
+    
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({error : error.message})
 })
+
+//router to delete avatar image
+router.delete('/users/me/avatar', auth, async(req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+
+}, (error, req, res, next) => {
+    res.status(400).send({error : "failed"})
+})
+
+//route to get user avatar
+router.get('/users/:id/avatar', async( req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)   
+
+    }
+    catch (e) {
+        res.status(404).send()
+    }
+}) 
 
 module.exports = router
