3cee93e59ee3058ee0e696e4ed048e0daebd513c
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2025-02-01 12:36:13 +0530

    multer for file upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+const multer = require('multer')
 
 router.post('/users', async (req, res) => {
 
@@ -137,6 +138,26 @@ router.delete('/users/me', auth, async (req, res) => {
     catch (e) {
         res.status(500).send(e)
     }
+})
+
+const upload = multer({
+    dest : 'avatars',
+    limits :{
+        fileSize : 1000000,
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/.(png|jpg|jpeg)$/gm)){
+            cb(new Error("please upload only png, jpg or jpeg"))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), auth, async ( req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error : error.message})
 })
 
 module.exports = router
